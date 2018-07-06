484ccc65e7a71827fb94ade42b43e580ecf5fe4f
rubik16x16 <drt_mike@hotmail.com>
2018-07-06 14:29:57 -0400

    crear lector de archivo .env

---
+++ boot.php
@@ -0,0 +1,36 @@
+<?php
+
+class env{
+
+  private $envVars;
+
+  public function __construct(){
+
+    $this->envVars= $this->readEnvFile();
+
+  }
+
+  private function readEnvFile(){
+
+    if(file_exists('.env')){
+      $envFile= fopen('.env', 'r');
+      while(($buffer= fgets($envFile)) != false){
+        list($k, $v)=(explode('=', $buffer));
+        $envVars[$k]= $v;
+      }
+      return $envVars;
+    }
+    throw new exception('Archivo .env no existe');
+  }
+
+  public function get($key){
+
+    if(isset($this->envVars[$key])){
+      return $this->envVars[$key];
+    }
+    throw new exception("La variable {$key} no esta definida en su archivo env");
+  }
+
+}
+
+$env= new env();
---
+++ index.php
@@ -0,0 +1,10 @@
+<span>abm basico</span>
+
+<a href="productos/">productos</a>
+
+<?php
+
+  require 'boot.php';
+  echo $env->get('DB_HOSTs');
+
+?>
